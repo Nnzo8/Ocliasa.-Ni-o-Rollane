2483300aec435316fc345081b8d455dbd6de4651
Nnzo8 <120240373+Nnzo8@users.noreply.github.com>
2023-10-25 13:12:53 +0800

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong
+{
+
+   public static void main(String []args)
+   { 
+      System.out.println("Passionate from miles away");
+      System.out.println("Passive with the things you say");
+      System.out.println("Passin' up on my old ways");
+      System.out.println("I can't blame you, no, no");
+   }
+}
---
+++ PRELIM/TablesAndChairs.java
@@ -0,0 +1,14 @@
+public class TablesAndChairs
+{
+
+   public static void main(String []args)
+   { 
+        System.out.println("x                    x");
+        System.out.println("x                    x");
+        System.out.println("x     xxxxxxxxxx     x");
+        System.out.println("xxxx  x        x  xxxx");
+        System.out.println("x  x  x        x  x  x");
+        System.out.println("x  x  x        x  x  x");
+
+      }
+   }
